665ac532957b7e28aa2e73dccab3846f29314369
최원빈 <chldnjsqls1264@gmail.com>
2019-08-07 21:52:31 +0900

    init project

---
+++ src/components/nav/gnb.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="gnb">
+        aa
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "gnb"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <gnb></gnb>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import gnb from '@/components/nav/gnb.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+    gnb
+  }
+}
+</script>
