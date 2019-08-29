8f7ca4387d33afd583bbc86b0c799f001d8af5ba
최원빈 <zad1264@naver.com>
2019-08-30 03:44:41 +0900

    add footer image changing & better visibility

--- src/components/Nav/fnb.vue
+++ src/components/Nav/fnb.vue
@@ -3,11 +3,18 @@
             app
             color="blue-grey"
             class="white--text"
+            height="40px"
     >
-        <span>타짜 ver.1.5.5 by. WonBeenC</span>
+        <span>타짜 ver.1.5.6 by. WonBeenC</span>
         <span style="float: right">
-            <img src="https://image.flaticon.com/icons/png/512/87/87390.png" class="footer__img" alt="instagram" @click="clickImg('https://www.instagram.com/choi_1been/?hl=ko')">
-            <img src="https://image.flaticon.com/icons/png/512/33/33702.png" class="footer__img" alt="facebook" @click="clickImg('https://www.facebook.com/profile.php?id=100007543098862')">
+            <img src="https://image.flaticon.com/icons/png/512/87/87390.png" class="footer__img"
+                 alt="instagram" @click="clickImg('https://www.instagram.com/choi_1been/?hl=ko')"
+                 onmouseover="this.src='https://sguru.org/wp-content/uploads/2018/01/photo-1.png'"
+                 onmouseout="this.src='https://image.flaticon.com/icons/png/512/87/87390.png'">
+            <img src="https://image.flaticon.com/icons/png/512/33/33702.png" class="footer__img" alt="facebook"
+                 @click="clickImg('https://www.facebook.com/profile.php?id=100007543098862')"
+                 onmouseover="this.src='https://image.flaticon.com/icons/png/512/124/124010.png'"
+                 onmouseout="this.src='https://image.flaticon.com/icons/png/512/33/33702.png'">
         </span>
     </v-footer>
 </template>
@@ -26,7 +33,7 @@
 
 <style scoped>
     .footer__img{
-        height: 18px;
+        height: 25px;
         padding-right: 20px;
     }
 </style>
